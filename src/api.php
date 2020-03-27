bfcd19a400421d93e58617be724323122895f8bc
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-03-27 00:08:45 -0300

    Ignore warnings about bad html
    https://stackoverflow.com/questions/1685277/warning-domdocumentloadhtml-htmlparseentityref-expecting-in-entity

--- src/api.php
+++ src/api.php
@@ -65,8 +65,15 @@ class MailmanAPI {
 		foreach($urlsForLetters as $url) {
 			$response = $this->client->request('GET', $url);
 
-			$dom = new \DOMDocument;
-			$dom->loadHTML($response->getBody());
+			$dom = new \DOMDocument('1.0', 'UTF-8');
+
+            // set error level
+            $internalErrors = libxml_use_internal_errors(true);
+
+            $dom->loadHTML($response->getBody());
+
+            // Restore error level
+            libxml_use_internal_errors($internalErrors);
 
 			$tables = $dom->getElementsByTagName("table")[4];
 			$trs = $tables->getElementsByTagName("tr");
